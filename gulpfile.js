e23889a29eb7e11df892e73589439b0e09c3e7f3
root <root@dev.syusin.ru>
2019-07-03 18:39:47 +0500

    Complete version (fixed bugs with gulp plugins)

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
 	concatCss = require('gulp-concat-css'),
 	cleanCSS = require('gulp-clean-css'),
 	concat = require('gulp-concat'),
-	// del = require('del'),
+	uglyfly = require('gulp-uglyfly'),
+	del = require('del'),
 	autoprefixer = require('gulp-autoprefixer');
 
 
@@ -17,17 +18,17 @@ var paths = {
 	}
 };
 
-// function clean() {
-// 	del([ 'assets' ]);
-// }
+function clean() {
+	return del([ 'assets' ]);
+}
 
 function css() {
 	return gulp.src(paths.css.src)
 		.pipe(concatCss("styles.min.css"))
-		// .pipe(autoprefixer({
-        //     browsers: ['last 2 versions'],
-        //     cascade: false
-        // }))
+		.pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
 		.pipe(cleanCSS({compatibility: 'ie8'}))
 		.pipe(gulp.dest(paths.css.dest));
 }
@@ -35,6 +36,7 @@ function css() {
 function js() {
 	return gulp.src(paths.js.src)
 		.pipe(concat("scripts.min.js"))
+		.pipe(uglyfly())
 		.pipe(gulp.dest(paths.js.dest));
 }
 
@@ -43,7 +45,7 @@ function watch() {
 	gulp.watch(paths.js.src, js);
 }
 
-var build = gulp.series(gulp.parallel(css, js));
+var build = gulp.series(clean, gulp.parallel(css, js));
 
 exports.watch = watch;
 exports.default = build;
