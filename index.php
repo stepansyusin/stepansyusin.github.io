d6f550880a50d1c9ae29988d4ab7d4bf9eb8cdcd
root <root@dev.syusin.ru>
2019-07-03 18:01:00 +0500

    Without animation

--- index.php
+++ index.php
@@ -34,6 +34,41 @@
 			</div>
 		</div>
 
+		<div class="max_width">
+			<div class="default_section_container">
+
+				<div class="default_section_container_left">
+					<div class="about_image"></div>
+					<?php if(isset($data) && isset($data['about_warning']) && strlen($data['about_warning']) > 0) {
+						echo '<div class="about_warning">'. $data['about_warning'] .'</div>';
+					} ?>
+				</div>
+
+				<div class="default_section_container_right">
+					<?php if(isset($data) && isset($data['about_title']) && strlen($data['about_title']) > 0) {
+						echo '<div class="about_title">'. $data['about_title'] .'</div>';
+					} ?>
+					<?php if(isset($data) && isset($data['about_description']) && strlen($data['about_description']) > 0) {
+						echo '<div class="about_description">'. $data['about_description'] .'</div>';
+					} ?>
+
+					<?php if(isset($data) && isset($data['about_adv']) && count($data['about_adv']) > 0) {
+						echo '<div class="about_adv">';
+
+						foreach($data['about_adv'] as $adv) {
+							echo '<div class="about_adv_item">
+									<div class="about_adv_item__image" style="background-image: url('. $adv['image'] .')"></div>
+									<div class="about_adv_item__text">'. $adv['title'].'</div>
+								</div>';
+						}
+						
+						echo '</div>';
+					} ?>
+				</div>
+
+			</div>
+		</div>
+
 
 	</section>
 
